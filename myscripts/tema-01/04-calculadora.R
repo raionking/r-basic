f6d6608df506e3ace75b3a7c90013e4130d6b4dd
DESKTOP-0AI4HU3\Edenilson <ruiz.edenilson@gmail.com>
2019-02-24 11:03:34 -0600

    mis scripts

---
+++ myscripts/tema-01/04-calculadora.R
@@ -0,0 +1,42 @@
+2*(3+5/2)
+
+2*((3+5)/2)
+
+2/3+5
+
+2%/%3+5
+
+2%%3
+
+# Dividendo = divisor * cociente + resto
+# D = d * q + r
+# r = D - d * q
+# q = D %/% d
+# r = D %% d
+
+
+sqrt(25)
+
+exp(1)
+
+log(pi)
+
+abs(-pi)
+
+factorial(4)
+
+choose(5, 3)
+
+
+sin(60*pi/180)
+/
+cos(60*pi/180)
+
+
+miVariable = 4
+doble = function(x){2*x}
+doble(miVariable)
+
+
+cuadrado = function(x){x^2}
+cuadrado(miVariable)
